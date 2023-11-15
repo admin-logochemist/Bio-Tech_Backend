d8c02a99d54c6535fe2468dec79220407a2c9f0f
Umar <umar.techsmiths@gmail.com>
2023-11-15 21:09:17 +0500

    commit

--- src/helpers/imageUpload.js
+++ src/helpers/imageUpload.js
@@ -1,10 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
+require("dotenv").config()
 
 cloudinary.config({
-  cloud_name: 'dswwhmznc',
-  api_key: '556174277448713',
-  api_secret: 'O0rGwtrV9dAKmzqOWxZDcusYhS0',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = multer.diskStorage({});
